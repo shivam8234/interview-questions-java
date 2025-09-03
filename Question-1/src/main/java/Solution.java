039dc3c95d4c81d2ff2e9b60a42bb3bddc6d887f
Jie Zhang <jie.zhang@nationsbenefits.com>
2025-09-03 10:09:35 -0700

    add questions

---
+++ Question-1/src/main/java/Solution.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+
+class Solution {
+
+    /*
+     * Complete the 'numberOfTokens' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER expiryLimit
+     *  2. 2D_INTEGER_ARRAY commands
+     */
+
+    public static int numberOfTokens(int expiryLimit, List<List<Integer>> commands) {
+        // Please implement this function
+        return -1;
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# interview-questions
+# interview
+
+## Java Coding Question
+
+* Question-1: [Authentication Tokens](Question-1/Question-1.md)
+* Question-2: [Product Service](Question-2/Question-2.md)
